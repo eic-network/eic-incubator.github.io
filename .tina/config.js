d5bca66c174381b66cffbf97f4fbf96f1a224786
Victor Nițu <victor@0x19.net>
2023-03-15 02:18:20 +0200

    fix env var name

--- .tina/config.js
+++ .tina/config.js
@@ -5,7 +5,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA_CLIENT_ID, // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID, // Get this from tina.io
   token: process.env.TINA_TOKEN, // Get this from tina.io
   build: {
     outputFolder: "admin",
